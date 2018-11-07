824df21671d16fa6f9a8dc5f556719817a6851b9
Raymond <raymond.yu171@gmail.com>
2018-11-07 15:34:57 -0500

    'script'

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -14,18 +14,20 @@ class Home extends Component {
 			<div id="container">
 				<div id="sales-pitch">
 					<p>
-						Free Shipping On Orders Over <span className="underline">$100</span>
+						Free Shipping On All Orders Over <span className="underline">$100</span>
 					</p>
 				</div>
 				<div id="banner">
 					<div id="banner-text">
-						<h1 id="company-name">Von Legacy</h1>
+						<h1>Von Legacy</h1>
 						<p>Premium Wear</p>
 					</div>
 					<img src={banner} />
 				</div>
-				<div>
-					<Link to="/login">Click Here To Login</Link>
+				<div id="wardrobe">
+					<div>
+						<Link to="/login">Become Click Here To Login</Link>
+					</div>
 				</div>
 			</div>
 		);
